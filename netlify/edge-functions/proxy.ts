044441f739df5d6c5a1feb110caf1df27d56562d
Sachsenspielt <sachsenspielt3@gmail.com>
2023-05-30 21:06:20 +0200

    feat: added cors

--- netlify/edge-functions/proxy.ts
+++ netlify/edge-functions/proxy.ts
@@ -1,5 +1,9 @@
 import type { Config, Context } from "https://edge.netlify.com";
 
+const headers = {
+    "Access-Control-Allow-Origin": "*",
+}
+
 export default async (request: Request, context: Context) => {
     const { url, headers } = await request.json();
 
@@ -7,11 +11,11 @@ export default async (request: Request, context: Context) => {
         headers
     });
 
-    if(req.status === 404) return new Response("Not Found", { status: 404 });
-    if(req.status === 401) return new Response("Unauthorized", { status: 401 });
-    if(req.status !== 200) return new Response("Error", { status: 500 });
+    if(req.status === 404) return new Response("Not Found", { status: 404, headers });
+    if(req.status === 401) return new Response("Unauthorized", { status: 401, headers });
+    if(req.status !== 200) return new Response("Error", { status: 500, headers });
 
-    return new Response(await req.text(), { status: 200 });
+    return new Response(await req.text(), { status: 200, headers });
 };
 
 export const config: Config = {
